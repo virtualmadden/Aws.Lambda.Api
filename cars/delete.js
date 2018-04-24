111ac031e4735ae2171eb720c385e9d490b4591e
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-04-23 18:14:09 -0700

    basic delete handler

---
+++ cars/delete.js
@@ -0,0 +1,39 @@
+const aws = require("aws-sdk");
+
+const dynamoClient = new aws.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+    try {
+        const params = {
+            TableName: process.env.DYNAMODB_TABLE,
+            Key: {
+                id: event.pathParameters.id
+            }
+        };
+
+        dynamoClient.delete(params, (error) => {
+            if (error) {
+                let response = {
+                    statusCode: error.statusCode || 501,
+                    headers: { 
+                        "Content-Type": "application/json"
+                    },
+                    body: {
+                        "error": error.message
+                    }
+                };
+
+              callback(null, response);
+            } else {
+                let response = {
+                    statusCode: 200,
+                    body: JSON.stringify({}),
+                };
+
+                callback(null, response);
+            }            
+        });
+    } catch (error) {
+        callback(error);
+    }   
+};
